1b5ef0242f69daa599c46524ec03bc3f0066e64a
Cica <codunlami>
2023-10-30 00:06:41 +0100

    first-commit

---
+++ expose-photographie/src/components/MyContext.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="component">
+      <h2 class="title">Contexte Historique et Artistique</h2>
+      <h3 class="sub-title">Contexte Historique</h3>
+      <p class="paragraph">
+        La série 'Les Mariés de Notre Époque' a été créée par Ishola Akpo en 2015, une époque où l'Afrique contemporaine était en pleine mutation sociale et culturelle.
+      </p>
+      <p class="paragraph">
+        Cette série explore la pratique de la dot, une tradition ancestrale qui continue à jouer un rôle central dans les mariages africains, malgré les changements rapides de la société.
+      </p>
+  
+      <h3 class="sub-title">Contexte Artistique</h3>
+      <p class="paragraph">
+        Ishola Akpo s'inspire des traditions africaines tout en introduisant des éléments modernes dans son travail, reflétant ainsi les défis et les opportunités de l'Afrique contemporaine. Ses photographies sont une fusion artistique de l'ancien et du nouveau.
+      </p>
+      <p class="paragraph">
+        Son travail est reconnu pour son esthétique unique et sa capacité à capturer des moments significatifs. Chacune de ses photos raconte une histoire et témoigne de son talent à saisir la profondeur des coutumes et des célébrations.
+      </p>
+    </div>
+  </template>
+  
+  <style scoped>
+  .component {
+    background-color: #f9f9f9;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    text-align: center;
+  }
+  
+  .title {
+    font-size: 28px;
+    color: #333;
+    margin-bottom: 20px;
+  }
+  
+  .sub-title {
+    font-size: 24px;
+    color: #007BFF;
+    margin-top: 20px;
+  }
+  
+  .paragraph {
+    font-size: 18px;
+    color: #555;
+    margin-bottom: 15px;
+    text-align: left;
+  }
+  </style>
+  
